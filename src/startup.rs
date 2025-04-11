b02ef880cd41cbe1f243a92efb3d44aa6143bba3
Filipe Santiago <porto.santiago@hotmail.com>
2025-04-11 14:16:25 -0300

    add tracing

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer, dev::Server, web};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -8,6 +9,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Register the connection as part of the application state
