c5df18345c3ae2b91219d5a91d9b0269cf84a66e
Filipe Santiago <porto.santiago@hotmail.com>
2025-04-14 18:42:34 -0300

    update database config to use ssl

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,10 @@
 use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::PgConnectOptions;
+use sqlx::{
+    ConnectOptions,
+    postgres::{PgConnectOptions, PgSslMode},
+};
 
 #[derive(Deserialize)]
 pub struct Settings {
@@ -25,19 +28,30 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 impl DatabaseSettings {
     pub fn with_db(&self) -> PgConnectOptions {
-        return self.without_db().database(&self.database_name);
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing_log::log::LevelFilter::Trace);
+
+        return options;
     }
 
     pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+
         return PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
             .password(&self.password.expose_secret())
-            .port(self.port);
+            .port(self.port)
+            .ssl_mode(ssl_mode);
     }
 }
 
