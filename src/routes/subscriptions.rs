a9716bdd698e1233db1800c8afbaaacc2421a61f
Filipe Santiago <porto.santiago@hotmail.com>
2025-04-21 09:22:10 -0300

    impl try_from

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,12 +4,23 @@ use serde::Deserialize;
 use sqlx::PgPool;
 use uuid::Uuid;
 
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+
+        return Ok(NewSubscriber { email, name });
+    }
 }
 
 #[tracing::instrument(
@@ -21,14 +32,9 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let new_subscriber = NewSubscriber {
-        email: form.0.email,
-        name,
     };
 
     return match insert_subscriber(&pool, &new_subscriber).await {
@@ -54,7 +60,7 @@ pub async fn insert_subscriber(
         VALUES($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
