1b038ef21ea119b67d068d2694cdff2ec6b7bee2
Filipe Santiago <porto.santiago@hotmail.com>
2025-05-01 11:12:56 -0300

    finish email tests

--- src/configuration.rs
+++ src/configuration.rs
@@ -6,10 +6,13 @@ use sqlx::{
     postgres::{PgConnectOptions, PgSslMode},
 };
 
+use crate::domain::SubscriberEmail;
+
 #[derive(Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
+    pub email_client: EmailClientSettings,
 }
 
 #[derive(Deserialize)]
@@ -17,6 +20,24 @@ pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
+}
+
+#[derive(Deserialize)]
+pub struct EmailClientSettings {
+    pub base_url: String,
+    pub sender_email: String,
+    pub authorization_token: Secret<String>,
+    pub timeout_milliseconds: u64,
+}
+
+impl EmailClientSettings {
+    pub fn sender(&self) -> Result<SubscriberEmail, String> {
+        return SubscriberEmail::parse(self.sender_email.clone());
+    }
+
+    pub fn timeout(&self) -> std::time::Duration {
+        return std::time::Duration::from_millis(self.timeout_milliseconds);
+    }
 }
 
 #[derive(Deserialize)]
